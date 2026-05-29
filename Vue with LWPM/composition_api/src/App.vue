96ff70fcae8fa0b9d9f2baa53235dc52f7bc41d1
RobiulHassan0 <robiulhassan270@gmail.com>
2026-05-29 22:00:26 +0600

    basic done

--- Vue with LWPM/composition_api/src/App.vue
+++ Vue with LWPM/composition_api/src/App.vue
@@ -1,23 +1,60 @@
 <script setup>
-import { ref } from 'vue';
-import useNumbers from './numbers';
+import { computed, ref } from 'vue';
 
-  let number = ref(0);
+  let password = ref("")
 
-  const {increment, decrement} = useNumbers(number)
+  const passwordStrength = computed( () => {
+    let strenth = (password.value.length * 100) / 10;
 
+    return Math.min(strenth, 100);
+  });
+
+  let firstName = ref("");
+  let lastName = ref("");
+
+  const fullName = computed( () => {
+    let fullName = firstName.value+ ' ' +lastName.value;
+
+    return fullName;
+  });
+
+
+  const zip = ref("");
+  
+  const zipUppercase = computed({
+    get: () => zip.value.toUpperCase(),
+
+    set: value => {zip.value = value.toUpperCase() }
+  });
 </script>
 
+
 <template>
+<div class="main">
 
-  <div>{{ number }}</div>
+  <div>
+      <input type="password" v-model="password" placeholder="Enter your password">
+      {{ passwordStrength }} % strong lol
+  </div>
 
-  <button @click="number++">Increment</button>
-  <button @click="number--">Decrement</button>
+  
+  <input type="text" v-model="firstName" placeholder="Enter your first name">
+  <input type="text" v-model="lastName" placeholder="Enter your last name">
+  {{ fullName }}
 
+  <br>
+
+  <input type="text" v-model="zipUppercase">
+  {{ zipUppercase}}
+    
+</div>
 </template>
 
 
 <style scoped>
-
+  .main{
+    display: flex;
+    justify-content: center;
+    gap: 50px;
+  }
 </style>
