e8d6354dca31580ddf4d19b26dfa4fb51b3671da
RobiulHassan0 <robiulhassan270@gmail.com>
2026-06-03 04:20:38 +0600

    learned vue router

---
+++ Vue with LWPM/composition_api/src/pages/About.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        This is About Page
+    </div>
+</template>
---
+++ Vue with LWPM/composition_api/src/router/index.js
@@ -0,0 +1,21 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: About
+    }
+];
+
+export default createRouter({
+    history: createWebHistory(),
+    routes
+})
