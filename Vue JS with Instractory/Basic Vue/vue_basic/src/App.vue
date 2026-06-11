d1554c8827b97a0b30603248b8908c792988e8f0
RobiulHassan0 <robiulhassan270@gmail.com>
2026-06-12 01:49:28 +0600

    vue pracitce

---
+++ Vue JS with Instractory/Basic Vue/vue_basic/src/App.vue
@@ -0,0 +1,39 @@
+<script >
+
+// Options API
+    // export default{
+    //   data(){
+    //     return{
+    //       courseName: 'Vue JS',
+    //       mentor: 'Mahmud',
+    //       count: 0
+    //     }
+    //   }
+    // }
+
+  export default{
+    setup(){
+      let count = 0;
+      const courseName = "Vue JS 3 Masterclass";
+      const mentor = "Mahmud Ibrahim";
+
+      return {count, courseName, mentor}
+    }
+  }
+
+</script>
+
+<template>
+  <h1>course name:  {{ courseName }}</h1>
+  <h1>Course By: {{ mentor }} </h1>
+
+  <button @click="count++">+</button>
+  <h4>{{ count }}</h4>
+  <button @click="count--">-</button>
+</template>
+
+
+
+
+
+<style scoped></style>
